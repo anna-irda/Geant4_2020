dfaaca3fe33c2fd07a288f253fca5f94cb457538
anna-irda <61865540+anna-irda@users.noreply.github.com>
2020-04-28 12:27:38 +0200

    Update SteppingAction.cc

--- PET/src/SteppingAction.cc
+++ PET/src/SteppingAction.cc
@@ -6,6 +6,7 @@
 //
 //
 #include "SteppingAction.hh"
+#include "G4SystemOfUnits.hh"
 #include "G4SteppingManager.hh"
 #include "G4Step.hh"
 
@@ -16,10 +17,41 @@ SteppingAction::SteppingAction():G4UserSteppingAction()
 
 SteppingAction::~SteppingAction() {}
 
-
 void SteppingAction::UserSteppingAction(const G4Step* theStep)
 {
-
+    G4Track* theTrack = theStep->GetTrack();
+    G4String volumeName = theTrack->GetVolume()->GetName();
+    
+    if (volumeName == "fantom")
+    {
+        auto secondaries = theStep -> GetSecondaryInCurrentStep();
+        nrOfSec+=secondaries->size();
+        std::cout << nrOfSec << std::endl;      
+    }
+    /*if (secondaries->size()>0)
+    {
+        std::cout << "size" << secondaries->size();
+        PrintStep(theStep);
+    }*/
 }
 
+void SteppingAction::PrintStep(const G4Step* theStep)
+{
+    
+    G4Track* theTrack = theStep->GetTrack();
+    G4String processName = theStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessName();
+    G4String particleName = theTrack->GetDefinition()->GetParticleName();
+    G4double energy = thTrack->GetKineticEnergy();
+    G4String volumeName = theTrack->GetVolume()->GetName();
+    G4double deltaEn = theStep->GetDeltaEnergy();
+    int trackNo = theTrack->GetTrackID();
 
+    std::cout << " trackNo: " << trackNo
+              << " particle: " << particleName
+              << " process: " << processName
+              << " kinetic energy: " << energy/MeV
+		          << " delta en: " << deltaEn
+		          << " voulme name: " << volumeName << std::endl;
+
+}
+int SteppingAction::nrOfSec=0;
