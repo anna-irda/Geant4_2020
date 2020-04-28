1bbd1057efeadc9ef3e3192bcd2d40ff1d629a08
anna-irda <61865540+anna-irda@users.noreply.github.com>
2020-04-28 13:37:56 +0200

    Update PET.hh

--- PET/include/PET.hh
+++ PET/include/PET.hh
@@ -18,6 +18,7 @@ class PET
     private:
     void ConstructCylinder(double r_minimum, double r_maximum, double length);
     void ConstructNaIDet(double radius, double NoOfDet);
+    void PET::ConstructSDandField()
     
     G4NistManager* man;
     G4LogicalVolume* cylinderLogVol;
