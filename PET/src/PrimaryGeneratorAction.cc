438efd20a7cc657394af73f3bb46ed6e41976ebb
anna-irda <61865540+anna-irda@users.noreply.github.com>
2020-04-28 11:30:15 +0200

    Update PrimaryGeneratorAction.cc

--- PET/src/PrimaryGeneratorAction.cc
+++ PET/src/PrimaryGeneratorAction.cc
@@ -3,7 +3,7 @@
 //
 /// \file PrimaryGeneratorAction.cc
 /// \brief Implementation of the PrimaryGeneratorAction class
-
+#include <math.h>
 #include "PrimaryGeneratorAction.hh"
 #include "G4ParticleGun.hh"
 #include "G4ParticleDefinition.hh"
@@ -37,6 +37,31 @@ void PrimaryGeneratorAction::SetUpDefault()
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {	
     particleGun->GeneratePrimaryVertex(anEvent);
-}	
+}
 
+void PrimaryGeneratorAction::GeneratePositronIncident(G4Event* anEvent)
+{
+    G4ParticleDefinition* particle = particleTable->FindParticle("e+");
+    particleGun->SetParticleDefinition(particle);			
+    particleGun->SetParticlePosition(G4ThreeVector(0.0*cm,0.0*cm,0.0*cm));
+    particleGun->SetParticleEnergy(600*keV);
+    particleGun->SetParticleMomentumDirection(GenerateIsotropicDirection());
+    particleGun->GeneratePrimaryVertex(anEvent);
+}
 
+void PrimaryGeneratorAction::GenerateBackgroundIncident(G4Event* anEvent)
+{
+    G4double worldX = 1.5*m;
+    G4double worldY = 1.5*m;
+    G4double worldZ = 1.5*m;
+    G4double randX = FindRandomFromHalfSize(worldX);
+    G4double randY = FindRandomFromHalfSize(worldY);
+    G4double randZ = FindRandomFromHalfSize(worldZ);
+    particleGun->SetParticlePosition(G4ThreeVector(randX,randY,randZ));
+    
+    G4ParticleDefinition* particle = particleTable->FindParticle("gamma");
+    particleGun->SetParticleDefinition(particle);
+    particleGun->SetParticleEnergy(1461*keV);
+    particleGun->SetParticleMomentumDirection(GenerateIsotropicDirection());
+    particleGun->GeneratePrimaryVertex(anEvent);
+}
