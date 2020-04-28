ea8568688ba71758b7e38df56db5ada4f2299e0b
anna-irda <61865540+anna-irda@users.noreply.github.com>
2020-04-28 12:55:03 +0200

    Update EventAction.cc

--- PET/src/EventAction.cc
+++ PET/src/EventAction.cc
@@ -7,14 +7,21 @@
 //
 #include "SteppingAction.hh"
 #include "EventAction.hh"
+#include "globals.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4SDManager.hh"
+#include "G4THitsMap.hh"
+#include "SpineHit.hh"
 
 EventAction::EventAction()
 {
-
+    //output.open("Secondaries.txt");
+    output.open("SpineEn.txt");
 }
  
 EventAction::~EventAction()
 {
+    output.close();
 }
 
 
@@ -26,6 +33,16 @@ void EventAction::BeginOfEventAction(const G4Event* anEvent)
 
 void EventAction::EndOfEventAction(const G4Event* anEvent)
 {
-    std::cout << SteppingAction::nrOfSec << std::endl; 
-    SteppingAction::nrOfSec=0;
+    //output << SteppingAction::nrOfSec << std::endl; 
+    //SteppingAction::nrOfSec=0;
+ 
+    G4int eventID = anEvent->GetEventID();
+    if( eventID % 10 == 0 )
+    {
+        G4cout << "Finished Running Event # " << eventID << G4endl;
+    }
+ 
+    outputFile<< eventID << " " << SteppingAction::totalEnergyDep << G4endl;
+    SteppingAction::totalEnergyDep = 0;
 }
+
